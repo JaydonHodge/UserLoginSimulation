cd272a2dd8f64c596b993fd2e761913763148b94
Jaydon Hodge <jaydonhodge@gmail.com>
2025-12-30 00:12:40 -0500

    Incorporated hashing into main

--- src/hash.ts
+++ src/hash.ts
@@ -1,14 +1,16 @@
 import { createHash } from 'crypto'
-import { Md5 } from 'ts-md5'
 
 function generateSHA256(input: string): string {
     return createHash('sha256').update(input).digest('hex')
 }
 
-const input: string = "steveslist"
 
-const out2 = generateSHA256("steveslist")
-const out1 = Md5.hashStr(input)
+const out1 = generateSHA256("Bob")
+const out2 = generateSHA256("Cheeks")
+const out3 = generateSHA256("Star")
+const out4 = generateSHA256("Pepe")
 
 console.log(`${out1}`)
 console.log(`${out2}`)
+console.log(`${out3}`)
+console.log(`${out4}`)
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
 import * as readline from 'node:readline/promises'
 import { stdin as input, stdout as output } from 'node:process'
+import { createHash } from 'crypto'
 
 // Hash-map for user credentials for now
 const creds = new Map<string, string>()
 
-creds.set("Doodle", "Bob")
-creds.set("Sandy", "Cheeks")
-creds.set("Patrick", "Star")
-creds.set("Tio", "Pepe")
+creds.set("Doodle", "cd9fb1e148ccd8442e5aa74904cc73bf6fb54d1d54d333bd596aa9bb4bb4e961")
+creds.set("Sandy", "e817313e058cf46a44e5e972d5e702fdf9efec145c46f00e8e0ee7197f006e35")
+creds.set("Patrick", "e357d396871d10121ee7921467cae8c0671d97275f7bb97e7ecfcf00970b5d27")
+creds.set("Tio", "e09b6c1fc25adca4c29229bd4e0fe1e2ed47ec1c5dd355c0aa8ecf4ce8d64041")
 
-function is_valid_credentials(username: string, password: string): boolean {
+function is_valid_credentials(username: string, unhashedPassword: string): boolean {
     if (creds.has(username)) {
-        if (password == creds.get(username)) {
-            console.log("YOU'RE IN !!... My Secret is that I sometime's don't lift the toilet seat...")
+        const password = createHash('sha256').update(unhashedPassword).digest('hex')
+        if (password === creds.get(username)) {
+            console.log("YOU'RE IN !!... My Secret is that sometimes I don't lift the toilet seat hehe")
             return true
         }
     }
