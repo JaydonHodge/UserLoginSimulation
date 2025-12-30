c03cd3779ca7557b15014f644c82b3616d8d28be
Jaydon Hodge <jaydonhodge@gmail.com>
2025-12-29 20:21:39 -0500

    Multiple user functionality

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,31 @@
-import * as readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
+import * as readline from 'node:readline/promises'
+import { stdin as input, stdout as output } from 'node:process'
+
+// Hash-map for user credentials for now
+const creds = new Map<string, string>()
+
+creds.set("Doodle", "Bob")
+creds.set("Sandy", "Cheeks")
+creds.set("Patrick", "Star")
+creds.set("Tio", "Pepe")
 
 function is_valid_credentials(username: string, password: string): boolean {
-    const correct_username: string = 'doodle';
-    const correct_password: string = 'bob';
-
-    if ((username == correct_username) && (password == correct_password)) {
-        console.log("Login Success!")
-        return true;
+    if (creds.has(username)) {
+        if (password == creds.get(username)) {
+            console.log("YOU'RE IN !!... My Secret is that I sometime's don't lift the toilet seat...")
+            return true
+        }
     }
-    console.log("GET LOSTTT!!!")
-    return false;
+
+    console.log("GET LOSTTT !!!")
+    return false
 }
 
-const rl = readline.createInterface({ input, output });
+const rl = readline.createInterface({ input, output })
 
-const username: string = await rl.question('What is your username?');
-const password: string = await rl.question('What is your password?');
+const username: string = await rl.question('What is your username?')
+const password: string = await rl.question('What is your password?')
 
-is_valid_credentials(username, password);
+is_valid_credentials(username, password)
 
-rl.close();
+rl.close()
