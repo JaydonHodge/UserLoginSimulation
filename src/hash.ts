3e40ad4118433905bcda7d0e64fba75d627b8312
Jaydon Hodge <jaydonhodge@gmail.com>
2025-12-30 19:45:42 -0500

    Changed to Argon2 hashing algorithm for optimal security. Also expanded hash.ts to automate adding users

--- src/hash.ts
+++ src/hash.ts
@@ -1,14 +1,134 @@
-import { createHash } from 'crypto'
+import argon2 from 'argon2'
 
-function generateSHA256(input: string): string {
-    return createHash('sha256').update(input).digest('hex')
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const credentialsPath = path.join(__dirname, '..', 'src', 'credentials.json');
+
+// Reading data
+const data = fs.readFileSync(credentialsPath, 'utf8');
+const creds = JSON.parse(data);
+
+async function generateArgon2(input: string): Promise<string> {
+    return (await argon2.hash(input))
 }
 
-function generateArgon2(input: string): string {
-    return createHash('argon2').update(input).digest('hex')
+const superheroCreds: { [key: string]: string } = {
+    'Bruce': 'Wayne',
+    'Clark': 'Kent',
+    'Diana': 'Prince',
+    'Peter': 'Parker',
+    'Tony': 'Stark',
+    'Steve': 'Rogers',
+    'Natasha': 'Romanoff',
+    'Barry': 'Allen',
+    'Wally': 'West',
+    'Hal': 'Jordan',
+    'Oliver': 'Queen',
+    'Arthur': 'Curry',
+    'Victor': 'Stone',
+    'Billy': 'Batson',
+    'Selina': 'Kyle',
+    'Carol': 'Danvers',
+    'Scott': 'Lang',
+    'Hank': 'Pym',
+    'Janet': 'VanDyne',
+    'Clint': 'Barton',
+    'Wade': 'Wilson',
+    'Matt': 'Murdock',
+    'Jessica': 'Jones',
+    'Luke': 'Cage',
+    'Danny': 'Rand',
+    'Frank': 'Castle',
+    'Stephen': 'Strange',
+    'Thor': 'Odinson',
+    'Loki': 'Laufeyson',
+    'Wanda': 'Maximoff',
+    'Pietro': 'Maximoff',
+    'Vision': 'Stark',
+    'James': 'Rhodes',
+    'Sam': 'Wilson',
+    'Bucky': 'Barnes',
+    'TChalla': 'Udaku',
+    'Shuri': 'Udaku',
+    'ScottS': 'Summers',
+    'Jean': 'Grey',
+    'Logan': 'Howlett',
+    'Ororo': 'Munroe',
+    'Kurt': 'Wagner',
+    'Remy': 'LeBeau',
+    'Rogue': 'Anna',
+    'Bobby': 'Drake',
+    'Kitty': 'Pryde',
+    'Piotr': 'Rasputin',
+    'Warren': 'Worthington',
+    'HankM': 'McCoy',
+    'Charles': 'Xavier',
+    'Erik': 'Lehnsherr',
+    'Kara': 'Danvers',
+    'Barbara': 'Gordon',
+    'Dick': 'Grayson',
+    'Jason': 'Todd',
+    'Tim': 'Drake',
+    'Damian': 'Wayne',
+    'Kyle': 'Rayner',
+    'John': 'Stewart',
+    'Guy': 'Gardner',
+    'JessicaC': 'Cruz',
+    'Ray': 'Palmer',
+    'Carter': 'Hall',
+    'Shiera': 'Sanders',
+    'Ted': 'Kord',
+    'Michael': 'Carter',
+    'Jaime': 'Reyes',
+    'Dinah': 'Lance',
+    'Zatanna': 'Zatara',
+    'JohnC': 'Constantine',
+    'Harley': 'Quinn',
+    'Pamela': 'Isley',
+    'Edward': 'Nygma',
+    'Oswald': 'Cobblepot',
+    'Harvey': 'Dent',
+    'Jonathan': 'Crane',
+    'Reed': 'Richards',
+    'Susan': 'Storm',
+    'Johnny': 'Storm',
+    'Ben': 'Grimm',
+    'Norrin': 'Radd',
+    'BruceB': 'Banner',
+    'Jennifer': 'Walters',
+    'Marc': 'Spector',
+    'Kamala': 'Khan',
+    'Miles': 'Morales',
+    'Gwen': 'Stacy',
+    'Miguel': 'OHara',
+    'Kate': 'Bishop',
+    'America': 'Chavez',
+    'Riri': 'Williams',
+    'Monica': 'Rambeau',
+    'Hope': 'VanDyne',
+    'Cassie': 'Lang',
+    'Yelena': 'Belova',
+    'Shang': 'Chi',
+    'Dane': 'Whitman',
+    'Thena': 'Eternal'
+};
+
+// Loop through new credentials and hash the passwords then add them to the config
+for (const username in superheroCreds) {
+    const password = superheroCreds[username]!  // ! Asserts that password is not undefined
+    const password_hashed = await generateArgon2(password)
+
+    creds.users.push({
+        username: username,
+        password_hash: password_hashed
+    })
 }
 
-
-const out1 = generateArgon2("Wayne")
-
-console.log(`${out1}`)
+// Write data back
+fs.writeFileSync(credentialsPath, JSON.stringify(creds, null, 2));
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import * as readline from 'node:readline/promises'
 import { stdin as input, stdout as output } from 'node:process'
-import { createHash } from 'crypto'
+import argon2 from 'argon2'
 
 import creds from './credentials.json' with {type: "json"}
 
-function is_valid_credentials(username: string, unhashedPassword: string): boolean {
-    const password = createHash('sha256').update(unhashedPassword).digest('hex')
+async function is_valid_credentials(username: string, unhashedPassword: string): Promise<boolean> {
+    const user = creds.users.find(user_f => username === user_f.username)
 
-    return (creds.users.some(user =>
-        username === user.username && password === user.password_hash))
+    if (user) {
+        if (await argon2.verify(user.password_hash, unhashedPassword))
+            return true
+    }
+    return false
 }
 
 
@@ -17,7 +20,7 @@ const rl = readline.createInterface({ input, output })
 const username: string = await rl.question('What is your username?')
 const password: string = await rl.question('What is your password?')
 
-if (is_valid_credentials(username, password)) {
+if (await is_valid_credentials(username, password)) {
     console.log("YOU'RE IN !!... My Secret is that sometimes I don't lift the toilet seat hehe")
 }
 else {
